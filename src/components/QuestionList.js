9e522403c968fe680929ffa3dd1c7daad5e5c3dc
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-11-29 11:05:32 +1100

    updated QuestionList and QuestionItem to fetch and display questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((questionData) => setQuestions(questionData))
+
+  }, [])
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => {
+        return <QuestionItem 
+          key={question.id}
+          question={question}
+        />
+      })}
+    </ul>
     </section>
   );
 }
